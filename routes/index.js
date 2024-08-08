ab1b405439640cc7b96798a2e7a83140f05f62cf
Myk78 <nwbyaseen@gmail.com>
2024-08-08 15:30:11 +0500

    set the register and login with authentication

--- routes/index.js
+++ routes/index.js
@@ -2,31 +2,88 @@ var express = require('express');
 var router = express.Router();
 const UserModel = require('./users');
 const PostModel = require('./post');
+const passport = require('passport');
+
+//these to line is used to login the user
+const localStrategy = require('passport-local');
+passport.authenticate(new localStrategy(UserModel.authenticate()));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/createuser', async function(req,res,next){
-  let user = await UserModel.create({
-    username: "Myk",
-    email: "nwbyas@gmail.com",
-    password: "yaseen123",
-    fullname: "Mohammad Yaseen",
-    posts: [],
-    timestamps: true 
+// register user route
+router.post('/register',function(req,res){
+  // let userdata = new UserModel({
+  //   username: req.body.username,
+  //   email: req.body.email,
+  //   fullname: req.body.fullname 
+  // });
+
+  // same as commented code above but it's more precise
+  let { username, email, fullname } = req.body;
+  let userdata = new UserModel({ username, email, fullname });
+  UserModel.register(userdata,req.body.password).then(function(){
+    passport.authenticate("local")(req,res,function(){
+      res.redirect('/profile');
+    });
   });
-  res.send(user);
 });
-router.get('/createpost', async function(req,res,next){
-  let post = await PostModel.create({
-    PostText: "Hello every ",
-    user:'66b3bed6807ca387a16db64d'
-  });
-  let user = await UserModel.findOne({_id:'66b3bed6807ca387a16db64d'});
-  user.posts.push(post._id);
-  await user.save();
-  res.send("Done");
+
+
+// login user
+router.post('/login',passport.authenticate("local",{
+  successRedirect:"/profile",
+  failureRedirect:"/"
+}),function(req,res){
+
 });
+
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Route to create user
+// router.get('/createuser', async function(req,res,next){
+//   let user = await UserModel.create({
+//     username: "Myk",
+//     email: "nwbyas@gmail.com",
+//     password: "yaseen123",
+//     fullname: "Mohammad Yaseen",
+//     posts: [],
+//     timestamps: true 
+//   });
+//   res.send(user);
+// });
+
+// // Route to show a userpost
+// router.get('/userpost',async function(req,res){
+//   let user = await UserModel.findOne({_id: "66b3bed6807ca387a16db64d"}).populate('posts');
+//   res.send(user);
+// })
+
+
+// // Route to create post
+// router.get('/createpost', async function(req,res,next){
+//   let post = await PostModel.create({
+//     PostText: "Hello every ",
+//     user:'66b3bed6807ca387a16db64d'
+//   });
+//   let user = await UserModel.findOne({_id:'66b3bed6807ca387a16db64d'});
+//   user.posts.push(post._id);
+//   await user.save();
+//   res.send("Done");
+// });
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const mongoose = require('mongoose');
+const plm = require('passport-local-mongoose');
 mongoose.connect("mongodb://127.0.0.1:27017/pinterest");
 
 // Define the schema for a user
@@ -37,6 +38,8 @@ const userSchema = new mongoose.Schema({
 }, {
   timestamps: true // Adds createdAt and updatedAt timestamps
 });
+
+userSchema.plugin(plm);
 
 // Create the model from the schema and export it
 module.exports = mongoose.model('User', userSchema);
